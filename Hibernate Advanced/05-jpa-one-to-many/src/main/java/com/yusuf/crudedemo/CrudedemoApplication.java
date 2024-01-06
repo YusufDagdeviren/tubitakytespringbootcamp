6aaa956b07661bdc67e276d206b0855460d9d41f
YusufDagdeviren <ddyusufdd@gmail.com>
2024-01-06 12:54:38 +0300

    Relationship between Instruction and Course one to many

---
+++ Hibernate Advanced/05-jpa-one-to-many/src/main/java/com/yusuf/crudedemo/CrudedemoApplication.java
@@ -0,0 +1,100 @@
+package com.yusuf.crudedemo;
+
+import com.yusuf.crudedemo.dao.AppDao;
+import com.yusuf.crudedemo.entity.Course;
+import com.yusuf.crudedemo.entity.Instructor;
+import com.yusuf.crudedemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudedemoApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDao appDao){
+		return runner -> {
+//			Ekleme İşlemini gerçekleştirken de cascade olduğu için instructor_detail'de silinecektir
+//			createInstructor(appDao);
+
+//			findInstructor(appDao);
+
+//			Silme İşlemini gerçekleştirken de cascade olduğu için instructor_detail'de silinecektir
+//			deleteInstructor(appDao);
+
+//			findInstructorDetail(appDao);
+//			Silme İşlemini gerçekleştirken de cascade olduğu için instructor'da silinecektir
+//			deleteInstructorDetail(appDao);
+
+//			Kurs ile birlikte Instructor_Detail'de beraber olmak üzere ekledik
+			createInstructorWithCourses(appDao);
+
+
+		};
+	}
+
+	private void createInstructorWithCourses(AppDao appDao) {
+		Instructor tempInstructor = new Instructor("Dusan","Tadic","salladim@gmail.com");
+		InstructorDetail instructorDetail = new InstructorDetail("http://www.youtube.com","play a football match");
+		tempInstructor.setInstructorDetail(instructorDetail);
+
+		// create some courses
+		Course tempCourse1 = new Course("How to make star stealing");
+		Course tempCourse2 = new Course("How to pass the ball");
+
+		// add courses to instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		// save the instructor
+		// Instructor'ı save yaptığımız zaman CascadeType.PERSIST olduğundan dolayı aynı zamanda Course ve InstructorDetail'de kaydolacak
+		System.out.println("Saving instructor: " + tempInstructor);
+		System.out.println("The courses: "+ tempInstructor.getCourses());
+		appDao.save(tempInstructor);
+		System.out.println("Done!");
+
+	}
+
+	private void deleteInstructorDetail(AppDao appDao) {
+		int id = 3;
+		System.out.println("Delete by id for InstructorId: "+id);
+		appDao.deleteInstructorDetailById(id);
+	}
+
+	private void findInstructorDetail(AppDao appDao) {
+		int id = 2;
+		InstructorDetail instructorDetail = appDao.findInstructorDetailById(id);
+		System.out.println("Instructor detail's detail : "+instructorDetail);
+		System.out.println("Instructor of Instructor_detail: "+instructorDetail.getInstructor());
+	}
+
+	private void deleteInstructor(AppDao appDao) {
+		int id = 1;
+		System.out.println("Delete by id for Instructor: "+id);
+		appDao.deleteInstructorById(id);
+
+	}
+
+	private void findInstructor(AppDao appDao) {
+		Instructor instructor = appDao.findInstructorById(2);
+		System.out.println(instructor);
+	}
+
+	private void createInstructor(AppDao appDao) {
+		Instructor tempInstructor = new Instructor("Dusan","Tadic","salladim@gmail.com");
+		InstructorDetail instructorDetail = new InstructorDetail("http://www.youtube.com","play a football match");
+		tempInstructor.setInstructorDetail(instructorDetail);
+
+		// Cascade all olduğu için aynı zamanda InstrutorDetail'e de kaydedilmiş olacak.
+
+		System.out.println("Saving instructor: "+ tempInstructor);
+		appDao.save(tempInstructor);
+		System.out.println("Done");
+	}
+}
